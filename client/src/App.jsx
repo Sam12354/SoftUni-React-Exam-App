fb9580a73fd904bea6fc31a9825fbe8fbeb1c123
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-02-27 11:49:04 +0200

    Adding Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import Footer from "./components/footer/Footer"
 
 function App() {
     return (
         <div id="page-top">
             <Header />
             <Home />
+            <Footer />
         </div>
     )
 }
