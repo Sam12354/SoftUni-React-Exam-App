09d25dd267cb6093ca6e32e5f7afc2cb4149366a
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-03-02 16:14:12 +0200

    setting up Login.jsx element

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../hooks/useAuth.js";
+import { useForm } from "../../hooks/useForm.js";
+
+const initialValues = { email: '', password: '' }
+
 export default function Login() {
+
+    const login = useLogin()
+
+    const navigate = useNavigate()
+
+    const loginHandler = async ({ email, password }) => {
+        try {
+            await login(email, password)
+            navigate('/')
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    const { values, changeHandler, onSubmit } = useForm(initialValues, loginHandler)
+
     return (
         <section className="page-section bg-light d-flex align-items-center justify-content-center vh-100"
         style={{ background: "url('/images/header-bg.jpg') center/cover no-repeat" }}>
@@ -9,14 +31,26 @@ export default function Login() {
                 <div className="row justify-content-center">
                     <div className="col-lg-6">
                         <div className="card shadow p-4 bg-white">
-                            <form action="/login" method="POST">
+                            <form onSubmit={onSubmit}>
                                 <div className="mb-3">
                                     <label htmlFor="email" className="form-label">Email Address</label>
-                                    <input type="email" className="form-control" id="email" name="email" required />
+                                    <input type="email" 
+                                    className="form-control" 
+                                    id="email" name="email" 
+                                    value={values.email} 
+                                    onChange={changeHandler}
+                                    required 
+                                    />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="password" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="password" name="password" required />
+                                    <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="password" name="password" 
+                                    value={values.password} 
+                                    onChange={changeHandler}  
+                                    required />
                                 </div>
                                 <button type="submit" className="btn btn-primary w-100">Login</button>
                                 <p className="text-center mt-3">Don't have an account? <a href="#register">Register here</a></p>
