937998aa4307481538af8e35298576956729f0e0
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-03-02 13:26:45 +0200

    hook for login, register, logout auth

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { login, register } from "../api/auth-api";
+
+
+export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
+    const loginHandler = async (email, password) => {
+        const result = await login(email, password); 
+
+        changeAuthState(result); 
+    };
+
+    return loginHandler;
+};
+
+export const useRegister = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
+    const registerHandler = async (email, password, rePass) => {
+        const result = await register(email, password, rePass); 
+        changeAuthState(result); 
+    };
+
+    return registerHandler;
+};
+
+export const useLogout = () => {
+    const { logout: localLogout } = useContext(AuthContext);
+
+    const logoutHandler = async () => {
+        await logout();  
+        localLogout();  
+    };
+
+    return logoutHandler;
+};
