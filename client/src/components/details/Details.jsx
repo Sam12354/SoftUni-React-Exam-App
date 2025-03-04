2381e6420dd91a67fd8e93e11720e0154014ff29
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-03-04 19:39:16 +0200

    doing details component

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,12 +1,16 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { useGetOneItem } from "../../hooks/useService"
 import { remove } from "../../api/api"
+import { Link } from "react-router-dom";
 
 export default function Details() {
     
     const { itemId } = useParams()
     const [data] = useGetOneItem(itemId);
     const item = data?.item || {};
+    const isOwner = data?.isOwner || false;
+    console.log(isOwner);
+    
 
     const navigate = useNavigate()
 
@@ -32,24 +36,29 @@ export default function Details() {
                                 <p>
                                     {item.description}
                                 </p>
-                                <ul className="list-inline">
-                                    <li><strong>Client:</strong> Threads</li>
-                                    <li><strong>Category:</strong> {item.brand}</li>
+                                <ul className="list-inline" style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+                                    <li><strong>Price:</strong> ${item.price}</li>
+                                    <li><strong>Brand:</strong> {item.brand}</li>
                                 </ul>
                                 <div className="d-flex gap-3">
-                                    <button className="btn btn-success btn-xl text-uppercase mt-3" type="button">
-                                        <i className="fas fa-shopping-cart me-1"></i> Buy
-                                    </button>
-                                    <button className="btn btn-warning btn-xl text-uppercase mt-3" type="button">
-                                        <i className="fas fa-edit me-1"></i> Edit
-                                    </button>
-                                    <button className="btn btn-danger btn-xl text-uppercase mt-3" type="button">
-                                        <i onClick={itemDeleteHandler} className="fas fa-trash me-1"></i> Delete
-                                    </button>
+                                    {isOwner ? (
+                                        <>
+                                            <Link to='#'>
+                                                <button className="btn btn-warning btn-xl text-uppercase mt-3" type="button">
+                                                    <i className="fas fa-edit me-1"></i> Edit
+                                                </button>
+                                            </Link>
+                                            <button className="btn btn-danger btn-xl text-uppercase mt-3" type="button">
+                                                <i onClick={itemDeleteHandler} className="fas fa-trash me-1"></i> Delete
+                                            </button>
+                                        </>
+                                        ) : (
+                                            <button className="btn btn-success btn-xl text-uppercase mt-3" type="button">
+                                                <i className="fas fa-shopping-cart me-1"></i> Buy
+                                            </button>
+                                        )
+                                    }
                                 </div>
-                                <button className="btn btn-primary btn-xl text-uppercase mt-3" type="button">
-                                    <i className="fas fa-arrow-left me-1"></i> Back to Catalog
-                                </button>
                             </div>
                         </div>
                     </div>
