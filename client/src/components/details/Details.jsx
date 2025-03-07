1e44191b37f7ef8bbfc943e62590d9fe9c51bb18
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-03-07 12:00:52 +0200

    fixing routes

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -2,13 +2,16 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useGetOneItem } from "../../hooks/useService"
 import { like, remove } from "../../api/api"
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Details() {
 
     const [isBought, setIsBought] = useState(false);
     const { itemId } = useParams()
     const [data] = useGetOneItem(itemId);
+
+    const { isAuthenticated } = useContext(AuthContext)
 
     const item = data?.item || {};
     const isOwner = data?.isOwner || false;
@@ -51,34 +54,38 @@ export default function Details() {
                                     <li><strong>Price:</strong> ${item.price}</li>
                                     <li><strong>Brand:</strong> {item.brand}</li>
                                 </ul>
-                                <div className="d-flex gap-3">
-                                    {isOwner ? (
-                                        <>
-                                            <Link to={`/${item._id}/edit`}>
-                                                <button className="btn btn-warning btn-xl text-uppercase mt-3" type="button">
-                                                    <i className="fas fa-edit me-1"></i> Edit
+
+                                {isAuthenticated ? 
+                                    <div className="d-flex gap-3">
+                                        {isOwner ? (
+                                            <>
+                                                <Link to={`/${item._id}/edit`}>
+                                                    <button className="btn btn-warning btn-xl text-uppercase mt-3" type="button">
+                                                        <i className="fas fa-edit me-1"></i> Edit
+                                                    </button>
+                                                </Link>
+                                                <button onClick={itemDeleteHandler} className="btn btn-danger btn-xl text-uppercase mt-3" type="button">
+                                                    <i className="fas fa-trash me-1"></i> Delete
                                                 </button>
-                                            </Link>
-                                            <button onClick={itemDeleteHandler} className="btn btn-danger btn-xl text-uppercase mt-3" type="button">
-                                                <i className="fas fa-trash me-1"></i> Delete
-                                            </button>
-                                        </>
-                                    ) : (
-                                        <>
-                                            {isBought ? (
-                                                <div className="alert alert-success text-uppercase mt-3" style={{ fontWeight: 'bold', fontSize: '1.5rem', padding: '0.5rem 1.5rem', borderRadius: '0.25rem', textAlign: 'center' }}>
-                                                    Item(s) have been successfully added to your cart.
-                                                </div>
-                                            ) : (
-                                                <button onClick={itemBuyHandler} className="btn btn-success btn-xl text-uppercase mt-3" type="button">
-                                                    <i className="fas fa-shopping-cart me-1"></i> Buy
-                                                </button>
-                                            )
-                                            }
-                                        </>
-                                    )
-                                    }
-                                </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                {isBought ? (
+                                                    <div className="alert alert-success text-uppercase mt-3" style={{ fontWeight: 'bold', fontSize: '1.5rem', padding: '0.5rem 1.5rem', borderRadius: '0.25rem', textAlign: 'center' }}>
+                                                        Item(s) have been successfully added to your cart.
+                                                    </div>
+                                                ) : (
+                                                    <button onClick={itemBuyHandler} className="btn btn-success btn-xl text-uppercase mt-3" type="button">
+                                                        <i className="fas fa-shopping-cart me-1"></i> Buy
+                                                    </button>
+                                                )
+                                                }
+                                            </>
+                                        )
+                                        }
+                                    </div>
+                                : 
+                                null }
                             </div>
                         </div>
                     </div>
