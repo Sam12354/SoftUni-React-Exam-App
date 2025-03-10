d570399cc7e80448f85921b2c6092ce0608ac021
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-03-10 20:08:56 +0200

    Update auth-api.js

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -2,16 +2,15 @@ import requester from "./requester.js"
 
 const BASE_URL = "http://localhost:7777/auth"
 
-export const login = (email, password) => {
+export const login = async (email, password) => {
 
-    const authData = requester.post(`${BASE_URL}/login`, { email, password })
-    // console.log(authData);
+    const authData = await requester.post(`${BASE_URL}/login`, { email, password })
     
     return authData
 }
 
-export const register = (email, password, rePass) => {
-    const authData = requester.post(`${BASE_URL}/register`, { email, password, rePass })
+export const register = async (email, password, rePass) => {
+    const authData = await requester.post(`${BASE_URL}/register`, { email, password, rePass })
 
     return authData
 
